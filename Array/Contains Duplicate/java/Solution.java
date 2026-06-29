be0e713874c966a920d1b5f5af68d71a0485c7ab
ILAKKIYA N <ilakkiyaind2005@gmail.com>
2026-06-29 10:58:24 +0530

    feat: Add Contains Duplicate

--- Array/Contains Duplicate/java/Solution.java
+++ Array/Contains Duplicate/java/Solution.java
@@ -3,9 +3,10 @@ class Solution {
        HashSet<Integer>set=new HashSet<>();
        for(int i=0;i<nums.length;i++)
        {
-       if(set.contains(i))
-       return false;
-       }
+       if(set.contains(nums[i]))
        return true;
+       set.add(nums[i]);
+       }
+       return false;
     }
 }
