74f831d7086e7683a0cdb271a29321c3335bd931
ILAKKIYA N <ilakkiyaind2005@gmail.com>
2026-06-29 12:11:12 +0530

    feat: Add Unique Morse Code Words

---
+++ Array/Unique Morse Code Words/java/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+
+        String[] morse = {
+            ".-","-...","-.-.","-..",".","..-.","--.","....","..",
+            ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+            "...","-","..-","...-",".--","-..-","-.--","--.."
+        };
+
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < words.length; i++) {
+
+            String code = "";
+
+            for (int j = 0; j < words[i].length(); j++) {
+
+                char ch = words[i].charAt(j);
+                code = code + morse[ch - 'a'];
+            }
+
+            set.add(code);
+        }
+
+        return set.size();
+    }
+}
--- README.md
+++ README.md
@@ -22,6 +22,7 @@ A professionally structured collection of topic-wise DSA solutions, optimized co
 | 217 | [Contains Duplicate](./Array/Contains%20Duplicate/) | Easy |
 | 287 | [Find the Duplicate Number](./Array/Find%20the%20Duplicate%20Number/) | Medium |
 | 349 | [Intersection of Two Arrays](./Array/Intersection%20of%20Two%20Arrays/) | Easy |
+| 804 | [Unique Morse Code Words](./Array/Unique%20Morse%20Code%20Words/) | Easy |
 | 977 | [Squares of a Sorted Array](./Array/Squares%20of%20a%20Sorted%20Array/) | Easy |
 | 996 | [Number of Squareful Arrays](./Array/Number%20of%20Squareful%20Arrays/) | Medium |
 
