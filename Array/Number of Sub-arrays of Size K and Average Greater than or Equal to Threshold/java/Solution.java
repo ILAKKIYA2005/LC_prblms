ac3e704c1e3351748ac169d1033f57e80cd2bebc
ILAKKIYA N <ilakkiyaind2005@gmail.com>
2026-06-30 12:25:57 +0530

    feat: Add Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold

---
+++ Array/Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold/java/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+       int left=0;
+       int right=0;
+       int current=0;
+       int count=0;
+       int ans=0;
+       while(right<arr.length)
+       {
+        current+=arr[right];
+        {
+            if(current/k>=threshold)
+            count++;
+        }
+
+
+        if(right-left+1==k)
+        {
+            ans=Math.max(ans,count);
+
+current-=arr[left];
+left++;
+        }
+       right++;
+       } 
+       return ans;
+    }
+}
